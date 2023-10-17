6824859e6bb12e00cbbb3535498175e9362584d2
PGuuu <117700245+PGuuu@users.noreply.github.com>
2023-10-17 12:35:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const ws = new WebSocket('ws://172.20.10.9:8080');
+
+function sendMessage() {
+  ws.send('Hello, server');
+}
+
+ws.onmessage = (event) => {
+  console.log(`Received: ${event.data}`);
+};
